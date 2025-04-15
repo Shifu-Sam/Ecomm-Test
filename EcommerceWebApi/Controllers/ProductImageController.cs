204c3f17f7f01bec2f38303fbf732348ae3db9ed
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-15 12:07:50 +0530

    Update ProductImageController.cs

--- EcommerceWebApi/Controllers/ProductImageController.cs
+++ EcommerceWebApi/Controllers/ProductImageController.cs
@@ -1,5 +1,6 @@
 using Ecomm_Database_Class.Model;
 using Ecomm_Database_Class.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
